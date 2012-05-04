44e0899cec49471d64aeaa111ebad5fb51919fc2
steve3279 <steve.clement@gmail.com>
2012-05-04 14:17:08 -0400

    fifth push

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable
-         :recoverable, :rememberable, :trackable, :validatable, :invitable
+  devise :invitable, :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable, :registerable, :invitable
   #removed :registerable
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
