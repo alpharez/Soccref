350a23c44523f0a2189d3a3043b610dfb8952cfa
steve3279 <steve.clement@gmail.com>
2012-05-04 14:19:12 -0400

    non-registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :registerable, :invitable
+         :recoverable, :rememberable, :trackable, :validatable, :invitable
   #removed :registerable
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
