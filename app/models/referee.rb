47750cd642287361fc897a0a0054031c815ec6b7
steve3279 <steve.clement@gmail.com>
2012-05-04 14:50:55 -0400

    cleaned up ref view

--- app/models/referee.rb
+++ app/models/referee.rb
@@ -2,12 +2,18 @@ class Referee < ActiveRecord::Base
   has_many :emails
   has_many :phones
 
+  default_scope :order => 'referees.city'
+
   def name
     firstname + " " + lastname
   end
 
   def location
     city + " " + state + " " + zip
+  end
+  
+  def ratings
+    "Center rating: " + crating.to_s + ", Line rating: " + lrating.to_s
   end
 
 end
